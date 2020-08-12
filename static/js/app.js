6663808bf9ad13ff5ba705a34f16d79238d6409c
collinsculley <collinsculley@yahoo.com>
2020-08-12 02:09:52 -0500

    challenge update

--- static/js/app.js
+++ static/js/app.js
@@ -30,34 +30,35 @@ var filters = {};
 function updateFilters() {
 
   // Save the element, value, and id of the filter that was changed
-  let date = d3.select("#datetime").property("value");
-  let city = d3.select("#city").property("value");
-  let state = d3.select("#state").property("value");
-  let country = d3.select("#country").property("value");
-  let shape = d3.select("#shape").property("value");
+  let date = ['datetime', d3.select("#datetime").property("value")];
+  let city = ['city', d3.select("#city").property("value")];
+  let state = ['state', d3.select("#state").property("value")];
+  let country = ['country', d3.select("#country").property("value")];
+  let shape = ['shape', d3.select("#shape").property("value")];
 
   // If a filter value was entered then add that filterId and value
   // to the filters list. Otherwise, clear that filter from the filters object
     if (date) {
-      filters = filters.append("date")
+      filters.date = [date[0], date[1]]
     }
-    else pass;
+    else filters = filters;
     if (city) {
-      filters = filters.append("city")
+      filters.city = [city[0], city[1]]
     }
-    else pass;
+    else filters = filters;
     if (state) {
-      filters = filters.append("state")
+      filters.state = [state[0], state[1]]
     }
-    else pass;
+    else filters = filters;
     if (country) {
-      filters = filters.append("country")
+      filters.country = [country[0], country[1]]
     }
-    else pass;
+    else filters = filters;
     if (shape) {
-      filters = filters.append("shape")
+      filters.shape = [shape[0], shape[1]]
     }
-    else pass;
+    else
+       delete filters.shape;
   // Call function to apply all filters and rebuild the table
   filterTable();
 }
@@ -68,9 +69,12 @@ function filterTable() {
   let filteredData = tableData;
 
   // loop through all of the filters and keep any data that matches the filter values
-  for (var i = 0; i < filters.length; i++) {
-    filteredData = filteredData.filter(row => row.filters[i] === (filters[i]("value")));
-  }
+  //for (var i = 0; i < filters.length; i++) {
+    //filteredData = filteredData.filter(row => row.filters[i] === (filters[i]("value")));
+  //}
+  Object.values(filters).forEach((element) => {
+    filteredData = filteredData.filter(row => row.element === element[1]);
+  });
 
   // Finally, rebuild the table using the filtered Data
   buildTable(filteredData);
@@ -78,7 +82,7 @@ function filterTable() {
 
 // Attach an event to listen for changes to each filter
 // Hint: You'll need to select the event and what it is listening for within each set of parenthesis
-d3.selectAll("#filter-btn").on("click", filterTable);
+d3.selectAll("#filter-btn").on("click", updateFilters);
 
 // Build the table when the page loads
 buildTable(tableData);
