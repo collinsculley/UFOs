f2b883cfc88836dd9ca2e895ab952f259c9c3d19
collinsculley <collinsculley@yahoo.com>
2020-08-16 17:10:57 -0500

    challenge complete, all filters functioning and looping

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ function buildTable(data) {
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-      console.log(cell.text(val));
+      //console.log(cell.text(val));
     });
   });
 }
@@ -42,57 +42,19 @@ function updateFilters() {
   filterTable();
 }
 
-// This function will replace your handleClick function
-// function updateFilters() {
-
-//   // Save the element, value, and id of the filter that was changed
-//   // let date = ['datetime', d3.select("#datetime").property("value")];
-//   // let city = ['city', d3.select("#city").property("value")];
-//   // let state = ['state', d3.select("#state").property("value")];
-//   // let country = ['country', d3.select("#country").property("value")];
-//   // let shape = ['shape', d3.select("#shape").property("value")];
-
-
-
-//   // If a filter value was entered then add that filterId and value
-//   // to the filters list. Otherwise, clear that filter from the filters object
-//     if (date) {
-//       filters.date = [date[0], date[1]]
-//     }
-//     else filters = filters;
-//     if (city) {
-//       filters.city = [city[0], city[1]]
-//     }
-//     else filters = filters;
-//     if (state) {
-//       filters.state = [state[0], state[1]]
-//     }
-//     else filters = filters;
-//     if (country) {
-//       filters.country = [country[0], country[1]]
-//     }
-//     else filters = filters;
-//     if (shape) {
-//       filters.shape = [shape[0], shape[1]]
-//     }
-//     else filters = filters;
-//   // Call function to apply all filters and rebuild the table
-//   filterTable();
-// }
-
 function filterTable() {
 
   // set the filteredData to the existing tableData
   let filteredData = tableData;
-
   // loop through all of the filters and keep any data that matches the filter values
-  //for (var i = 0; i < filters.length; i++) {
-    //filteredData = filteredData.filter(row => row.filters[i] === (filters[i]("value")));
-  //};
-  Object.values(filters).forEach(([key,value]) => {
+  //console.log(filters)
+  
+  Object.entries(filters).forEach(([key,value]) => {
+    //console.log('key: ', key, 'value: ', value)
     filteredData = filteredData.filter(row => row[key] === value);
   });
-
+  
+  //console.log(filteredData);
   //Use .slice above!
 
   // Finally, rebuild the table using the filtered Data
